7af05104c805ae3ec2935f91640548ac080d26a8
Joakim Hansen <42582449+571164@users.noreply.github.com>
2020-02-09 18:02:01 +0100

    message

--- dat110-project1/src/no/hvl/dat110/messaging/Message.java
+++ dat110-project1/src/no/hvl/dat110/messaging/Message.java
@@ -9,7 +9,8 @@ public class Message {
 	private byte[] payload;
 
 	public Message(byte[] payload) {
-		this.payload = payload; // TODO: check for length within boundary
+		if (payload.length < MessageConfig.SEGMENTSIZE)
+			this.payload = payload; // TODO: check for length within boundary
 	}
 
 	public Message() {
@@ -17,31 +18,37 @@ public class Message {
 	}
 
 	public byte[] getData() {
-		return this.payload; 
+		return this.payload;
 	}
 
 	public byte[] encapsulate() {
-		
+
 		byte[] encoded = null;
-		
+
 		// TODO
 		// encapulate/encode the payload of this message in the
 		// encoded byte array according to message format
-		
-		if (true)
-		   throw new UnsupportedOperationException(TODO.method());
 
+		// Sets the size of the encoded array to 128bits
+		encoded = new byte[MessageConfig.SEGMENTSIZE];
+		// Sets the first bit to the size of the payload(data message)
+		encoded[0] = (byte) payload.length;
+		// Copy the content of the payload into the encoded array
+		System.arraycopy(payload, 0, encoded, 1, payload.length);
 		return encoded;
-		
+
 	}
 
 	public void decapsulate(byte[] received) {
 
 		// TODO
-		// decapsulate the data contained in the received byte array and store it 
+		// decapsulate the data contained in the received byte array and store it
 		// in the payload of this message
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		// Updating size of payload to the size of the received-array
+		payload = new byte[received[0]];
+		// Copy the content of the payload into the encoded array
+		System.arraycopy(received, 1, payload, 0, payload.length);
+
 	}
 }
